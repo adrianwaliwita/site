a8eb551bdccb8b07b88526b31333ecb0765b4f0b
adrianwaliwita <adwaliwita@gmail.com>
2025-06-03 13:09:09 +0530

    - tag manager added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 // ✅ NO "use client"
 import "./globals.css";
 import localFont from "next/font/local";
+import Script from "next/script"; // ✅ Add this
 import ClientLayout from "./ClientLayout";
 
 const GTAmerica = localFont({
@@ -44,7 +45,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${GTAmerica.variable}`}>
+      <head>{/* ✅ Put regular <link>, <meta>, etc. here */}</head>
       <body>
+        {/* ✅ GTM script must be inside body */}
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N2NN3B88');
+            `,
+          }}
+        />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2NN3B88"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
